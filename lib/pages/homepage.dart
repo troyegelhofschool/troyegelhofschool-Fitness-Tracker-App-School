500005641628915b9bd0786d6f592b66edf23803
troyegelhofschool <140989924+troyegelhofschool@users.noreply.github.com>
2024-09-09 20:23:21 +1200

    before school

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/data/workout_data.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +12,18 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Consumer<WorkoutData>(
+      builder: (context, value, child) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Workout Tracker'),
+        ),
+        body: ListView.builder(
+          itemCount: value.getWorkoutList().length,
+          itemBuilder: (context, index) => ListTile(
+            title: Text(value.getWorkoutList()[index].name),
+          ),
+        ),
+      ),
+    );
   }
 }
